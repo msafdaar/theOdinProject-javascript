7d1bd7d7e699b8c32a092466237162b7b06d6552
Safdar <71539665+malta-khan@users.noreply.github.com>
2022-08-17 17:18:06 +0500

    Squashed commit of the following:
    commit 5d13005d6e12f75fd47ac169819c841bd537171f
    Author: Safdar <71539665+malta-khan@users.noreply.github.com>
    Date:   Wed Aug 17 17:17:31 2022 +0500
    
        remove giphy api practice
    
    commit 10678f78009c755ba35be31842ea0fdf232b6bb1
    Author: Safdar <71539665+malta-khan@users.noreply.github.com>
    Date:   Wed Aug 17 17:14:56 2022 +0500
    
        add helper functions, remove test code
    
    commit e41c8f68497a5cc10056f88053324bbdd7de1d20
    Author: Safdar <71539665+malta-khan@users.noreply.github.com>
    Date:   Wed Aug 17 12:54:39 2022 +0500
    
        Update index.js
    
    commit 6e7e1a70bc0be35069ac5ad42b2dcc36b1c97256
    Author: Safdar <71539665+malta-khan@users.noreply.github.com>
    Date:   Wed Aug 17 12:51:54 2022 +0500
    
        added js to openweather
    
    commit 3649399df5052bcc77ac13b3f61dafe3566a2256
    Author: Safdar <71539665+malta-khan@users.noreply.github.com>
    Date:   Wed Aug 17 12:06:34 2022 +0500
    
        added help material
    
    commit 1b05a438ca5e49f7fcf8a9006319fb8747f860c9
    Author: Safdar <71539665+malta-khan@users.noreply.github.com>
    Date:   Wed Aug 17 12:02:29 2022 +0500
    
        start openweather api

---
+++ openweathermap/index.js
@@ -0,0 +1,78 @@
+document.querySelector("#searchButton").addEventListener("click", searchWeather)
+document.querySelector("#searchText").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.querySelector("#searchButton").click();
+    }
+  });
+
+
+function searchWeather(){
+searchText = document.querySelector("#searchText").value;
+fetchWeather(searchText)
+.then((response)=>{loadWeather(response)})
+}
+
+//fetch from api
+function fetchWeather(city){
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=066c24b1dbef5e109839495c0f1bdec7&units=metric`)
+  .then((response)=> response.json())
+}
+
+//load fetched data to dom using template string
+function loadWeather(apiResponse){
+  let container = document.querySelector("#weatherContainer")
+  container.innerHTML = ""
+  container.innerHTML = `
+  <div id="weatherHead">
+      <div>
+          <div id="cityName">${apiResponse.name}, ${apiResponse.sys.country}</div>
+          <div>${apiResponse.main.temp}, Feels like ${apiResponse.main.feels_like}</div>
+      </div>
+          <figure>
+              <img src="http://openweathermap.org/img/wn/${apiResponse.weather[0].icon}@2x.png" alt="weather-icon" id="weatherIcon">
+          <figcaption>${apiResponse.weather[0].description}</figcaption>
+          </figure>
+  </div>
+  <div class="weatherSection">
+      <h2>Details</h2>
+      <div>Humidity: ${apiResponse.main.humidity}%</div>
+      <div>Clouds: ${apiResponse.clouds.all}%</div>
+      <br>
+      <div>Wind Direction: ${getCardinal(apiResponse.wind.deg)}</div>
+      <div>Wind Speed: ${apiResponse.wind.speed} Kmph</div>
+      <div>Wind Gust: ${apiResponse.wind.gust} Kmph</div>
+  </div>
+  <div class="weatherSection">
+      <h2>Time</h2>
+      <div>Local Time in ${apiResponse.name} (GMT ${apiResponse.timezone/3600}): ${localTime(apiResponse.timezone)}</div>
+      <br>
+      <div>Sunrise: ${new Date(apiResponse.sys.sunrise*1000).toTimeString()}</div>
+      <br>
+      <div>Sunset: ${new Date(apiResponse.sys.sunset*1000).toTimeString()}</div>
+  </div>
+`
+}
+
+// Copied from Github
+function getCardinal(angle) {
+    const degreePerDirection = 360 / 8;
+    const offsetAngle = angle + degreePerDirection / 2;
+  
+    return (offsetAngle >= 0 * degreePerDirection && offsetAngle < 1 * degreePerDirection) ? "N"
+      : (offsetAngle >= 1 * degreePerDirection && offsetAngle < 2 * degreePerDirection) ? "NE"
+        : (offsetAngle >= 2 * degreePerDirection && offsetAngle < 3 * degreePerDirection) ? "E"
+          : (offsetAngle >= 3 * degreePerDirection && offsetAngle < 4 * degreePerDirection) ? "SE"
+            : (offsetAngle >= 4 * degreePerDirection && offsetAngle < 5 * degreePerDirection) ? "S"
+              : (offsetAngle >= 5 * degreePerDirection && offsetAngle < 6 * degreePerDirection) ? "SW"
+                : (offsetAngle >= 6 * degreePerDirection && offsetAngle < 7 * degreePerDirection) ? "W"
+                  : "NW";
+  }
+
+// Copied from stackoverflow
+function localTime(timezoneInSecs){
+    var d = new Date();
+    var utc = d.getTime();
+    var localDate = new Date(utc + (1000*timezoneInSecs));
+    return localDate.toUTCString().replace("GMT", "")
+}
